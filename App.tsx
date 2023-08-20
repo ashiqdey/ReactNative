b4181de7bf628fd36366a6b09fa7e105413de0d2
ashiqdey <ashiqdey@gmail.com>
2023-08-20 10:19:49 +0530

    internal styles

--- App.tsx
+++ App.tsx
@@ -5,26 +5,42 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
+  Text,
   View,
+  Button,
 } from 'react-native';
 
 function App(): JSX.Element {
+  const [clicked, setClicked] = useState(false);
   // const isDarkMode = useColorScheme() === 'dark';
+
+  const callfunc = () => {
+    setClicked(!clicked);
+  };
 
   return (
     <View style={{backgroundColor: 'pink'}}>
-      <Text style={{fontWeight: 700, color: 'red'}}>Hello</Text>
+      <Text style={styles.text}>
+        Hello {clicked ? 'Clicked' : 'not clciked'}
+      </Text>
+      <Button title="Submit" onPress={callfunc} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    backgroundColor: 'pink',
+    color: 'blue',
+  },
+});
 
 export default App;
